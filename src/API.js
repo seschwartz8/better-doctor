c958bc7bc6590ad01719cc4cb4d5ee06474a5be3
Sarah Schwartz <seschwartz8@gmail.com>
2020-02-14 11:29:47 -0800

    API calls for specific sypmtoms

--- src/API.js
+++ src/API.js
@@ -2,13 +2,11 @@ let API = '02e57727a2bacdfb5d79e0d822f8544c';
 
 export function callAPISymptom(symptomInput) {
   let url =
-    'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' +
+    `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptomInput}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=` +
     API;
-  console.log('symptom ' + symptomInput);
 
   fetch(url)
     .then(function(response) {
-      console.log(response);
       return response.json();
     })
     .then(function(data) {
