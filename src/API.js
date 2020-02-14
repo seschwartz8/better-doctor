8e04a79088e306d1b8858bcbd0314807fd5ab296
Sarah Schwartz <seschwartz8@gmail.com>
2020-02-14 12:51:37 -0800

    attempting to use environment variables for API_KEY but unable

--- src/API.js
+++ src/API.js
@@ -1,4 +1,25 @@
-let API = '02e57727a2bacdfb5d79e0d822f8544c';
+let API_KEY = '02e57727a2bacdfb5d79e0d822f8544c';
+// let API_KEY = process.env.API_KEY;
+
+function getDoctors(json) {
+  let doctors = [];
+  for (let doctor of json.data) {
+    let firstName = doctor.profile.first_name;
+    let lastName = doctor.profile.last_name;
+    let newPatients = doctor.practices.some(acceptingNew);
+    let imgUrl = doctor.profile.image_url;
+    let contact = getContact(doctor);
+    let doctorObj = {
+      firstName: firstName,
+      lastName: lastName,
+      newPatients: newPatients,
+      imgUrl: imgUrl,
+      contact: contact
+    };
+    doctors.push(doctorObj);
+  }
+  return doctors;
+}
 
 function acceptingNew(practice) {
   return practice.accepts_new_patients && practice.within_search_area
@@ -27,31 +48,14 @@ function getContact(doctor) {
 }
 
 export function callAPISymptom(symptomInput) {
-  let url =
-    `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptomInput}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=` +
-    API;
+  let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptomInput}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${API_KEY}`;
 
   fetch(url)
     .then(function(response) {
       return response.json();
     })
     .then(function(json) {
-      let doctors = [];
-      for (let doctor of json.data) {
-        let firstName = doctor.profile.first_name;
-        let lastName = doctor.profile.last_name;
-        let newPatients = doctor.practices.some(acceptingNew);
-        let imgUrl = doctor.profile.image_url;
-        let contact = getContact(doctor);
-        let doctorObj = {
-          firstName: firstName,
-          lastName: lastName,
-          newPatients: newPatients,
-          imgUrl: imgUrl,
-          contact: contact
-        };
-        doctors.push(doctorObj);
-      }
+      const doctors = getDoctors(json);
       console.log(doctors);
     });
 }
