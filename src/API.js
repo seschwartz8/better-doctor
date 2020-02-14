c27a81e994c0f181edd7c319c6331855386d3e47
Sarah Schwartz <seschwartz8@gmail.com>
2020-02-14 10:09:11 -0800

    collect user input into variables and pass them into API calls in separate file

---
+++ src/API.js
@@ -0,0 +1,7 @@
+export function callAPISymptom(symptomInput) {
+  console.log(`I call an API with ${symptomInput}`);
+}
+
+export function callAPIName(nameInput) {
+  console.log(`I call an API with ${nameInput}`);
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from 'jquery';
+import $ from 'jquery';
+import { callAPISymptom, callAPIName } from './API';
+
+$(document).ready(function() {
+  $('form.sypmtom').submit(function(event) {
+    event.preventDefault();
+
+    let sypmtomInput = $('#symptom-input').val();
+    callAPISymptom(sypmtomInput);
+  });
+
+  $('form.name').submit(function(event) {
+    event.preventDefault();
+
+    let nameInput = $('#name-input').val();
+    callAPIName(nameInput);
+  });
+});
