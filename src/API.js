63d05b43b52e9dcbf9ea6233f1b5de2c8a8992af
Sarah Schwartz <seschwartz8@gmail.com>
2020-02-14 11:24:18 -0800

    get API call working

--- src/API.js
+++ src/API.js
@@ -1,5 +1,19 @@
+let API = '02e57727a2bacdfb5d79e0d822f8544c';
+
 export function callAPISymptom(symptomInput) {
-  console.log(`I call an API with ${symptomInput}`);
+  let url =
+    'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' +
+    API;
+  console.log('symptom ' + symptomInput);
+
+  fetch(url)
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    });
 }
 
 export function callAPIName(nameInput) {
